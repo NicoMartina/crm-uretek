cfd14f4342d5574c5b438491d5e5cfd5b0dbbc92
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-02-19 08:19:25 -0300

    refactor: remove unused Budget, Interaction, Expense, ProjectEnum and StatsService

--- crmuretek/src/main/java/com/crmuretek/crmuretek/repositories/BudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crmuretek.crmuretek.repositories;
-
-import com.crmuretek.crmuretek.models.Budget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
-}
--- crmuretek/src/main/java/com/crmuretek/crmuretek/repositories/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crmuretek.crmuretek.repositories;
-
-import com.crmuretek.crmuretek.models.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, String> {
-}
