a624b0d63815a972de2311361e93331657ed1cf4
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-02-19 23:13:19 -0300

    refactor: clean up InventoryController and DashboardController - remove redundant @CrossOrigin annotations - replace @Autowired field injection with constructor injection in DashboardController - remove unused imports - remove AI generated comments

--- crmuretek/src/main/java/com/crmuretek/crmuretek/controllers/InventoryController.java
+++ crmuretek/src/main/java/com/crmuretek/crmuretek/controllers/InventoryController.java
@@ -1,17 +1,14 @@
 package com.crmuretek.crmuretek.controllers;
 
 import com.crmuretek.crmuretek.models.Inventory;
-import com.crmuretek.crmuretek.repositories.InventoryRepository;
 import com.crmuretek.crmuretek.services.InventoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/inventory")
-@CrossOrigin(origins = "*")
 public class InventoryController {
 
 
