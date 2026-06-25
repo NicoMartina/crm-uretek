15b0ed05dfd2c932f942de754829e8fdc87e7d45
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-06-25 08:58:38 -0300

    fix: fixed duplicate error when editing consults.

--- crmuretek-frontend/src/types/LeadUpdateData.ts
+++ crmuretek-frontend/src/types/LeadUpdateData.ts
@@ -1,5 +1,16 @@
+
+
 export interface LeadUpdateData {
   problemDescription: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  contactChannel: LeadContactEnum;
+  source: SourceEnum;
+  contactDate: LocalDate;
+  title: TitleEnum;
+  observations: string;
   customer: {
     id?: number;
   };
