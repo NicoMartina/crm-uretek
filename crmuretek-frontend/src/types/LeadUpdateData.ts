4c84edb1a7da8565000fe88175896af4d976ccff
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-06-25 12:00:04 -0300

    fix: fixed TypeScript  enum  in LeadUpdateData to use string.

--- crmuretek-frontend/src/types/LeadUpdateData.ts
+++ crmuretek-frontend/src/types/LeadUpdateData.ts
@@ -1,15 +1,13 @@
-
-
 export interface LeadUpdateData {
   problemDescription: string;
   name: string;
   email: string;
   phoneNumber: string;
   address: string;
-  contactChannel: LeadContactEnum;
-  source: SourceEnum;
-  contactDate: LocalDate;
-  title: TitleEnum;
+  contactChannel: string;
+  source: string;
+  contactDate: string;
+  title: string;
   observations: string;
   customer: {
     id?: number;
