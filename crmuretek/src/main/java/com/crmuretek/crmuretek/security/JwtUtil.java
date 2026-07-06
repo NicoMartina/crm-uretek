afdb669efcf47f20d52552156aaa030b66ee1958
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-07-06 17:15:50 -0300

    feat: add JwtUtil for token  generation  and validation

---
+++ crmuretek/src/main/java/com/crmuretek/crmuretek/security/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.crmuretek.crmuretek.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final SecretKey secretKey = Jwts.SIG.HS256.key().build();
+    private final long EXPERIENCE_MS = 1000 * 60 * 60 * 10; // 10 hrs
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() +  EXPERIENCE_MS))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+
+}
