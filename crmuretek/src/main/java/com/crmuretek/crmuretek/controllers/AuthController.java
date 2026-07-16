095733e367d8d9da725e2093c20dc3791a852ee5
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-07-16 15:00:18 -0300

    chore: remove temporarysetup and debug endpoints.

--- crmuretek/src/main/java/com/crmuretek/crmuretek/controllers/AuthController.java
+++ crmuretek/src/main/java/com/crmuretek/crmuretek/controllers/AuthController.java
@@ -6,10 +6,7 @@ import com.crmuretek.crmuretek.repositories.UserRepository;
 import com.crmuretek.crmuretek.security.JwtUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -27,14 +24,16 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginRequestDTO request){
         Optional<User> optionalUser = userRepository.findByUsername(request.getUsername());
 
+        User user = optionalUser.get();
+        System.out.println(">>> DB password: " + user.getPassword());
+        System.out.println(">>> Matches: " + passwordEncoder.matches(request.getPassword(), user.getPassword()));
+        boolean passwordMatches = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
 
         if (optionalUser.isEmpty()) {
             return ResponseEntity.status(401).build();
 
         }
-
-        User user = optionalUser.get();
-        boolean passwordMatches = passwordEncoder.matches(request.getPassword(), user.getPassword());
 
         if (!passwordMatches){
             return ResponseEntity.status(401).build();
