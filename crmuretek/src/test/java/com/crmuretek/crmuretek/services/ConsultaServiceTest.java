15f44ebbc8b87c756bbdafa0c55914e098bed17d
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-07-21 15:57:26 -0300

    test: add unit tests for PresupuestoService

--- crmuretek/src/test/java/com/crmuretek/crmuretek/services/ConsultaServiceTest.java
+++ crmuretek/src/test/java/com/crmuretek/crmuretek/services/ConsultaServiceTest.java
@@ -16,8 +16,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
