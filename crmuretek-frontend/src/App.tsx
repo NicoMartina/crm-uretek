507206aebac2c04dea393f1027a776e3720c3113
nicolas.martina69@gmail.com <nicolas.martina69@gmaill.como>
2026-07-23 07:37:36 -0300

    fix: send consultaId as flat field when creating job

--- crmuretek-frontend/src/App.tsx
+++ crmuretek-frontend/src/App.tsx
@@ -1197,7 +1197,7 @@ export default function App() {
               } else {
                 await jobService.create({
                   ...data,
-                  consulta: { id: selectedLead.consultaId || selectedLead.id },
+                  consultaId: selectedLead.consultaId || selectedLead.id,
                 });
               }
               setIsAddingQuote(false);
